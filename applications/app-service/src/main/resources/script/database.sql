05803c35ddfa7a0f3f095c13b2fb0362f1ba28f5
Darwin Gomez <dagomezp@ufpso.edu.co>
2025-09-09 22:34:12 -0500

    - Se modifica database.sql con el fin de que se creen las tablas correspondientes solo sino existen. - Se mueve el Dockerfile al directorio deployment

--- applications/app-service/src/main/resources/script/database.sql
+++ applications/app-service/src/main/resources/script/database.sql
@@ -1,16 +1,11 @@
-DROP TABLE IF EXISTS user_entity;
-DROP TABLE IF EXISTS rol;
 
-SELECT * FROM rol;
-SELECT * FROM user_entity;
-
-CREATE TABLE rol (
+CREATE TABLE IF NOT EXISTS rol (
 				    id SERIAL PRIMARY KEY,
 				    nombre VARCHAR(255) NOT NULL UNIQUE,
 				    descripcion VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE user_entity (
+CREATE TABLE IF NOT EXISTS user_entity (
                          id BIGSERIAL PRIMARY KEY,
                          nombres VARCHAR(255) NOT NULL,
                          apellidos VARCHAR(255) NOT NULL,
@@ -29,4 +24,10 @@ INSERT INTO rol (id, nombre, descripcion)
 VALUES
     (1, 'ADMIN', 'Control total del sistema'),
     (2, 'ASESOR', 'Gestión de clientes y asesoría'),
-    (3, 'CLIENTE', 'Acceso limitado a su información');
+    (3, 'CLIENTE', 'Acceso limitado a su información')
+ON CONFLICT (id) DO NOTHING;
+
+/*
+   $2a$10$qEHTkA51qs0C/q2gRtWWoOTDAg5pKh65URD5qcb196Vv5rsiGElPu
+   Darwin10.
+*/
